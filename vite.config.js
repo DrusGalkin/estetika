ed9429ebef1301692c55affc1362f0f7f6c720c0
unknown <sveta.galkina2021@gmail.com>
2025-10-03 15:15:10 +0300

    fix2

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    viteCommonjs()
+  ],
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+      include: [/wowjs/, /node_modules/]
+    }
+  },
+  optimizeDeps: {
+    include: ['wowjs']
+  }
 })
